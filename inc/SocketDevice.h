6c477e8fd145d7e08f0cbfdb819d908f56e2aee8
chris <chba6499@colorado.edu>
2018-09-03 18:42:58 -0600

    Initial commit

---
+++ inc/SocketDevice.h
@@ -0,0 +1,16 @@
+/*
+SocketDevice.h
+Christopher Bate
+September 2018
+
+This is meant to be a layer to abstract away the actual socket calls. In any cross-platform extentions, this would be an interface 
+that would be implemented by different platform-specific classes (Unix, Windows, etc).
+For this project, we don't bother with an interface and instead just add some basic unix socket functionalit.
+*/
+#ifndef SOCKET_DEVICE_H
+#define SOCKET_DEVICE_H
+
+
+
+
+#endif
